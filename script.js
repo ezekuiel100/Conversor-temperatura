3f148b027ccff435dfb8f399191455810418302b
unknown <ezekuiel100@gmail.com>
2024-01-09 10:11:42 -0300

    Refactoring

--- script.js
+++ script.js
@@ -1,46 +1,34 @@
 let temperatura = document.querySelector("#temperatura");
-const botaoConverter = document.querySelector("button");
-let deTemp = document.querySelector(".deTemp");
-let paraTemp = document.querySelector(".paraTemp");
+let unidadeOrigem = document.querySelector(".unidade-origem-select");
+let unidadeDestino = document.querySelector(".unidade-destino-select");
 let resultado = document.querySelector(".resultado");
 
+const botaoConverter = document.querySelector("button");
+
+let conversoes = {
+  CF: (temperatura) => (temperatura * 9) / 5 + 32,
+  FC: (temperatura) => (temperatura - 32) * (5 / 9),
+  CK: (temperatura) => temperatura + 273.15,
+  FK: (temperatura) => (temperatura - 32) * (5 / 9) + 273.15,
+  KC: (temperatura) => temperatura - 273.15,
+  KF: (temperatura) => (temperatura - 273.15) * (9 / 5) + 32,
+};
+
 botaoConverter.addEventListener("click", () => {
-  let unidade = deTemp.value + paraTemp.value;
-  converterTemperatura(unidade, temperatura.value);
+  let unidade = unidadeOrigem.value + unidadeDestino.value;
+  let tempValor = Number(temperatura.value);
+  converterTemperatura(unidade, tempValor);
 });
 
 function converterTemperatura(unidade, temperatura) {
   if (temperatura == "") return;
 
-  let temp;
-  temperatura = Number(temperatura);
+  let funcaoConversao = conversoes[unidade];
 
-  switch (unidade) {
-    case "CF":
-      temp = (temperatura * 9) / 5 + 32;
-      resultado.textContent = `${temp.toFixed(2)}°${paraTemp.value}`;
-      break;
-    case "FC":
-      temp = (temperatura - 32) * (5 / 9);
-      resultado.textContent = `${temp.toFixed(2)}°${paraTemp.value}`;
-      break;
-    case "CK":
-      temp = temperatura + 273.15;
-      resultado.textContent = `${temp.toFixed(2)}°${paraTemp.value}`;
-      break;
-    case "FK":
-      temp = (temperatura - 32) * (5 / 9) + 273.15;
-      resultado.textContent = `${temp.toFixed(2)}°${paraTemp.value}`;
-      break;
-    case "KC":
-      temp = temperatura - 273.15;
-      resultado.textContent = `${temp.toFixed(2)}°${paraTemp.value}`;
-      break;
-    case "KF":
-      temp = (temperatura - 273.15) * (9 / 5) + 32;
-      resultado.textContent = `${temp.toFixed(2)}°${paraTemp.value}`;
-      break;
-    default:
-      resultado.textContent = `${temperatura}°${paraTemp.value}`;
+  if (funcaoConversao) {
+    let tempFinal = funcaoConversao(temperatura);
+    resultado.textContent = `${tempFinal.toFixed(2)}°${unidadeDestino.value}`;
+  } else {
+    resultado.textContent = `${temperatura}°${unidadeDestino.value}`;
   }
 }
